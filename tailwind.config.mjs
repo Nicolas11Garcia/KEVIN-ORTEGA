e7b03ee8a81fc35941e425aea088ff8dbc81f333
Nicolas11Garcia <nicolasgarciajimenez12@gmail.com>
2024-08-13 17:20:45 -0400

    up

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  prefix: "",
+  darkMode: 'false',
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        yellow: "#f2bc39",
+        negro: "#1A2128",
+        parrafo: "#434D56"
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
